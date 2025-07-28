c0326313a205fb051f257378e666100587a8dfef
AntonioTagliafierro <anto.tagliafierro@studenti.unina.it>
2025-07-28 23:52:46 +0200

    creazione route per l'aggiunta di dati base per un annuncio (da aggiungere ulteriori dati) condivisione di HttpClient tra github e geoapify

--- src/main/kotlin/com/plugins/Routing.kt
+++ src/main/kotlin/com/plugins/Routing.kt
@@ -2,11 +2,15 @@ package com.plugins
 
 import com.*
 import com.data.models.admin.AdminDataSource
+import com.data.models.property.MongoPropertyDataSource
 import com.data.models.user.UserDataSource
 import com.security.hashing.HashingService
 import com.security.token.GitHubOAuthService
 import com.security.token.TokenConfig
 import com.security.token.TokenService
+import com.service.GeoapifyService
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -17,8 +21,15 @@ fun Application.configureRouting(
     tokenService: TokenService,
     tokenConfig: TokenConfig,
     gitHubOAuthService: GitHubOAuthService,
-
+    httpClient: HttpClient
 ) {
+
+    val propertyDataSource = MongoPropertyDataSource(getDatabase())
+
+    val geoapifyService = GeoapifyService(
+        apiKey = System.getenv("GEOAPIFY_KEY"),
+        httpClient = HttpClient(CIO)
+    )
     routing {
         userAuth( hashingService,userDataSource,tokenService, tokenConfig)
         signUpAdmin(hashingService, adminDataSource)
@@ -32,6 +43,7 @@ fun Application.configureRouting(
             tokenConfig
         )
         state()
+        propertyRoutes(propertyDataSource, geoapifyService)
     }
 }
 
