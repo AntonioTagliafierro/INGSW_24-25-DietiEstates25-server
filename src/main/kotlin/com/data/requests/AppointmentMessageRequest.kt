196d25bc48e0ae98a932b5158fe06451b3d53e3b
AntonioTagliafierro <anto.tagliafierro@studenti.unina.it>
2025-10-18 00:18:55 +0200

    fix

--- src/main/kotlin/com/data/requests/AppointmentMessageRequest.kt
+++ src/main/kotlin/com/data/requests/AppointmentMessageRequest.kt
@@ -2,12 +2,13 @@ package com.data.requests
 
 import kotlinx.serialization.Serializable
 import org.litote.kmongo.serialization.LocalDateSerializer
+import com.data.models.user.User
 import java.time.LocalDate
 
 @Serializable
 data class AppointmentMessageRequest(
     val appointmentId: String,
-    val senderId: String,
+    val sender: User,
     @Serializable(with = LocalDateSerializer::class)
     val date: LocalDate
 )
